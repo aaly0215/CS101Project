187a3086d41c92cf61c68068a1da69493a308f59
aaly0215 <aaly0215@gmail.com>
2022-03-26 13:02:16 -0700

    added if statements to correctly place boats

--- Main/boards.py
+++ Main/boards.py
@@ -9,6 +9,7 @@ class Board:
     def __init__(self, column = column, row = row) -> None:
         #automatically create new board with list comprehension
         self.board = [['O' for x in range(column)] for x in range(row)]
+        self.coordinates = []
 
     def show_playboard(self) -> None:
         for b in self.board:
@@ -20,20 +21,34 @@ class Board:
         col_choice = random.randint(1, column)
         print(row_choice)
         print(col_choice)
-        boat_size = input('How long is the boat? ')
-        boat_direction = input('Horizontal or Vertical? ').lower()
-        if boat_direction == 'horizontal':            
-            counter = 0
-            for x in range(int(boat_size)):                
-                self.board[int(row_choice )- 1][int(col_choice) - 1 + counter] = '0'
-                counter += 1
+        boat_size = random.randint(2, 7)
+        boat_direction = random.choice(['horizontal', 'vertical'])
+        if boat_direction == 'horizontal':
+            if boat_size + col_choice <= row:
+                counter = 0
+                for x in range(int(boat_size)):   
+                    self.coordinates.append([int(row_choice)- 1, int(col_choice) - 1 + counter])             
+                    self.board[int(row_choice )- 1][int(col_choice) - 1 + counter] = '0'
+                    counter += 1
+                    
+            else:
+                print('This boat went out of bounds!')
+                self.place_boat()
 
-
-        if boat_direction == 'vertical': 
-            counter2 = 0           
-            for x in range(int(boat_size)):
-                self.board[int(row_choice )- 1 + counter2][int(col_choice) - 1] = '0'
-                counter2 += 1
+        
+        elif boat_direction == 'vertical':
+            if boat_size + row_choice <= column:
+                counter2 = 0           
+                for x in range(int(boat_size)):
+                    self.coordinates.append([int(row_choice )- 1 + counter2, int(col_choice) - 1])
+                    self.board[int(row_choice )- 1 + counter2][int(col_choice) - 1] = '0'
+                    counter2 += 1
+                    
+            else:
+                    print('This boat went out of bounds!')
+                    self.place_boat()
+        else:
+            print('You need to pick Horizontal or Vertical.')
         
   
 
--- Main/main.py
+++ Main/main.py
@@ -2,8 +2,9 @@ from boards import *
 from ships import *
 
 board1 = Board()
-ship1 = Ship(1)
-board1.place_boat()
+
+ship1 = board1.place_boat()
 
 board1.show_playboard()
+print(board1.coordinates)
     
