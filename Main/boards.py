530d9806711ba0b08f72fca7381bcfe71f2636fb
aaly0215 <aaly0215@gmail.com>
2022-03-29 11:40:51 -0700

    added gameloop logic, made shooting possible

--- Main/boards.py
+++ Main/boards.py
@@ -1,58 +1,72 @@
 import random
 
+
+
+def pick_int(question: str)-> int:
+    while True:
+        answer = input(question)   
+        try:
+            return int(answer)
+        except ValueError:
+            print('You have to pick a number!')
+        
+
+
 class Board:
     #class veri to lock in board size
     row = 9
     column = 10
-    new_board = []
+    hits = 0
         
     def __init__(self, column = column, row = row) -> None:
         #automatically create new board with list comprehension
         self.board = [['O' for x in range(column)] for x in range(row)]
         self.coordinates = []
+        
 
     def show_playboard(self) -> None:
         for b in self.board:
             print(*b)
 
     def place_boat(self, row = row, column = column) -> None:
-        #pick a spot on current board
+        #picks a spot on current board
         row_choice = random.randint(1, row)
-        col_choice = random.randint(1, column)
-        print(row_choice)
-        print(col_choice)
+        col_choice = random.randint(1, column)        
         boat_size = random.randint(2, 7)
         boat_direction = random.choice(['horizontal', 'vertical'])
         if boat_direction == 'horizontal':
             if boat_size + col_choice <= row:
-                counter = 0
+                counter = 0            
                 for x in range(int(boat_size)):   
                     self.coordinates.append([int(row_choice)- 1, int(col_choice) - 1 + counter])             
-                    self.board[int(row_choice )- 1][int(col_choice) - 1 + counter] = '0'
+                    #self.board[int(row_choice )- 1][int(col_choice) - 1 + counter] = '*'
                     counter += 1
                     
-            else:
-                print('This boat went out of bounds!')
+            else:                
                 self.place_boat()
 
         
         elif boat_direction == 'vertical':
             if boat_size + row_choice <= column:
-                counter2 = 0           
+                counter2 = 0                
                 for x in range(int(boat_size)):
                     self.coordinates.append([int(row_choice )- 1 + counter2, int(col_choice) - 1])
-                    self.board[int(row_choice )- 1 + counter2][int(col_choice) - 1] = '0'
+                    #self.board[int(row_choice )- 1 + counter2][int(col_choice) - 1] = '*'
                     counter2 += 1
                     
-            else:
-                    print('This boat went out of bounds!')
+            else:                
                     self.place_boat()
+
+    def shoot(self) -> None:
+        x = pick_int('Pick a row ')
+        y = pick_int('Pick a column ')
+        shot_coordinates = [x - 1, y - 1]
+        if shot_coordinates in self.coordinates:
+            print('You hit my ship!')
+            self.board[x - 1][y - 1] = '*'
         else:
-            print('You need to pick Horizontal or Vertical.')
-        
-  
+            print('You missed!')
+            self.board[x - 1][y - 1] = 'X'
 
-
-
-
-       
+    def count_hits(self) -> None:
+        pass
--- Main/main.py
+++ Main/main.py
@@ -2,7 +2,6 @@ from boards import *
 from ships import *
 
 board1 = Board()
-
 ship1 = board1.place_boat()
 
 board1.show_playboard()
