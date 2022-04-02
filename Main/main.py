2956fcfbf3c6bcfb653feab62ac33031df3bd9ad
aaly0215 <aaly0215@gmail.com>
2022-04-02 14:54:46 -0700

    added delayed terminal clear for QOL

--- Main/main.py
+++ Main/main.py
@@ -1,5 +1,6 @@
 from boards import *
-
+from os import system
+import time
 
 board1 = Board()
 turns = 30
@@ -12,11 +13,14 @@ for amt in range(amt_ships):
 
 #gameloop
 while board1.hits < len(board1.coordinates) and turns > 0:
+    time.sleep(1.5)
+    system("cls")    
     board1.show_playboard()
-    print(f'You have {turns} turns left.')
+    print(f'You have {turns} turns left.')    
     board1.shoot()
     turns -= 1
     board1.hits = board1.count_hits()
+    
     
     
 if board1.hits == len(board1.coordinates):
